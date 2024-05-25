4e9e3247d045220d3a6b2713144c8c5477466ac3
Gabriel C. Brandão <biel.brandao2004@gmail.com>
2024-05-24 21:48:05 -0300

    feat(parse): add expression parsing

--- packages/grimoire/src/expression.rs
+++ packages/grimoire/src/expression.rs
@@ -1,6 +1,6 @@
 use errors::{DungeonError, DungeonResult};
 use pest::Parser;
-use query::ast::Expression;
+use query::ast::{Expression, GetExpr};
 
 use crate::{
     literal::parse_literal,
@@ -16,17 +16,24 @@ pub fn parse_expression(input: &str) -> DungeonResult<Expression> {
         .into_inner()
         .next()
         .ok_or(DungeonError::new("Could not get expression value"))?;
-    println!("{:#?}", inner_value);
     match inner_value.as_rule() {
         Rule::literal => Ok(Expression::Literal(parse_literal(inner_value.as_str())?)),
-        Rule::get_expr => todo!(),
+        Rule::get_expr => {
+            let ast_key = inner_value
+                .into_inner()
+                .next()
+                .ok_or(DungeonError::new("Could not parse get expression"))?;
+            Ok(Expression::Get(GetExpr {
+                key: ast_key.as_str().to_owned(),
+            }))
+        }
         _ => unreachable!(),
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use query::ast::{Expression, Literal};
+    use query::ast::{Expression, GetExpr, Literal};
 
     use super::parse_expression;
 
@@ -34,5 +41,15 @@ mod tests {
     fn test_parse_literal() {
         let parsed = parse_expression("1").unwrap();
         assert_eq!(parsed, Expression::Literal(Literal::Integer(1)));
+    }
+    #[test]
+    fn test_parse_get_expr() {
+        let parsed = parse_expression("get name").unwrap();
+        assert_eq!(
+            parsed,
+            Expression::Get(GetExpr {
+                key: "name".to_owned()
+            })
+        );
     }
 }
