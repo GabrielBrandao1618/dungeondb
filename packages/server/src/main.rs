499ce51963ed6132c989a41ea5ae364c4eaed5f0
Gabriel C. Brandão <biel.brandao2004@gmail.com>
2024-06-02 17:41:54 -0300

    feat(server): enable clients to run multiple commands per connection

--- packages/server/src/main.rs
+++ packages/server/src/main.rs
@@ -23,16 +23,17 @@ async fn main() -> io::Result<()> {
         let chest = chest.clone();
         let handle: io::Result<()> = tokio::spawn(async move {
             let (mut r, mut w) = stream.split();
-            let mut input: Vec<u8> = Vec::new();
-            let _ = r.read_buf(&mut input).await?;
-            let parsed = parse(&String::from_utf8_lossy(&input))
-                .map_err(|err| io::Error::new(ErrorKind::InvalidData, err))?;
-            let mut chest_lock = chest.lock().await;
-            let result = runner::run_query(&mut *chest_lock, parsed)
-                .map_err(|err| io::Error::new(ErrorKind::InvalidData, err))?;
-            w.write(format!("{}\n", result.to_string()).as_bytes())
-                .await?;
-            Ok(())
+            loop {
+                let mut input: Vec<u8> = Vec::new();
+                let _ = r.read_buf(&mut input).await?;
+                let parsed = parse(&String::from_utf8_lossy(&input))
+                    .map_err(|err| io::Error::new(ErrorKind::InvalidData, err))?;
+                let mut chest_lock = chest.lock().await;
+                let result = runner::run_query(&mut *chest_lock, parsed)
+                    .map_err(|err| io::Error::new(ErrorKind::InvalidData, err))?;
+                w.write(format!("{}\n", result.to_string()).as_bytes())
+                    .await?;
+            }
         })
         .await?;
         if let Err(err) = handle {
