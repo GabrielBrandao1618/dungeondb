7823f6f24131f1e737eebda800a0e28b9f9c5d51
Gabriel C. Brandão <biel.brandao2004@gmail.com>
2024-07-17 06:04:35 -0300

    feat(server): improve server error handling

--- packages/server-value/src/lib.rs
+++ packages/server-value/src/lib.rs
@@ -19,7 +19,7 @@ impl ServerError {
 
 impl std::fmt::Display for ServerError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "Server error")
+        write!(f, "{}", self.msg)
     }
 }
 
