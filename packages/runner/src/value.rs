018f6dc803633be9177d7d438ac7ada2fe9a5fbd
Gabriel C. Brandão <biel.brandao2004@gmail.com>
2024-07-21 19:25:47 -0300

    refactor(runner): remove unused code from runner

--- packages/runner/src/value.rs
+++ packages/runner/src/value.rs
@@ -1,15 +1,6 @@
 use chest::value::Value;
 use query::ast::Literal;
 
-pub fn value_to_query(src: Value) -> Literal {
-    match src {
-        Value::Integer(v) => Literal::Integer(v),
-        Value::Float(v) => Literal::Float(v),
-        Value::String(v) => Literal::String(v),
-        Value::Boolean(v) => Literal::Boolean(v),
-        Value::Invalid => unreachable!(),
-    }
-}
 pub fn value_from_query(lit: Literal) -> Value {
     match lit {
         Literal::String(v) => Value::String(v),
