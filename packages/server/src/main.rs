df6f7efa7b8ca17c5ff01e854f846230c10f35a4
Gabriel C. Brandão <biel.brandao2004@gmail.com>
2024-07-26 06:02:51 -0300

    feat(server): use environment variable in server instead of cli arg

--- packages/server/src/main.rs
+++ packages/server/src/main.rs
@@ -7,8 +7,7 @@ use server::Server;
 
 #[tokio::main]
 async fn main() -> io::Result<()> {
-    let mut args = env::args();
-    let port = args.nth(1).unwrap_or("3000".to_owned());
+    let port = env::var("PORT").unwrap_or("3000".to_owned());
     let mut server = Server::default();
     println!("Running server on port {port}");
     server.start(format!("localhost:{port}")).await?;
